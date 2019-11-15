ae260ccbe0624154160ef9c3ce8cba7f40f576aa
root <root@localhost.localdomain>
2019-11-15 07:44:43 -0500

    added

--- full_API/project2/routes/dishRouter.js
+++ full_API/project2/routes/dishRouter.js
@@ -66,7 +66,7 @@ dishRouter.route('/:dishId')
 })
 .post((req,res,next)=>{
     res.setHeader = 403;
-    res.end('PUT request is not supported');
+    res.end('POST request is not supported');
 })
 .put((req,res,next)=>{
     Dishes.findByIdAndUpdate(req.params.dishId,{
@@ -90,80 +90,85 @@ dishRouter.route('/:dishId')
 });
 
 
+// adding functionality for the comments section
+// adding functionality for the comment section
+// start the mongodb with the data folder and start working
 
 
-
-dishRouter.route('/')
-.get((req,res,next) => {
-    Dishes.find({})
-    .then((dishes) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(dishes);
-        //if there is an error they will be passed to the next
-        // cause we pass it so at the end if there is
-        // a final error handler we will catch it and
-        // do waterever it happend
-    }, (err) => next(err)).catch((err)=>next(err));
-    // everytime we get error we just pass it
+dishRouter.route('/:dishId/comments') // fetch all the comments in a 
+                                        // single dish object
+.get((req,res,next)=>{
+    // first you have to find the dish
+    // then get the comments
+    Dishes.findById(req.params.dishId)
+    // we get the dish
+    .then((dish)=>{
+        // if there is no dish then just stop but if not null then precees
+        if(dish !=null){
+            /// send the status code
+            res.statusCode = 200;
+            res.setHeader('Content-Type','application/json');
+            res.json(dish.comments);
+        }
+    })
 })
 .post((req,res,next)=>{
-    Dishes.create(req.body)
+    Dishes.findById(req.params.dishId)
+    //if you find one
     .then((dish)=>{
-        console.log("Dish is created",dish);
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json(dish);
-    },(err)=>next(err)).catch((err)=>next(err));
+            // you have to post the comment of a specfic dish
+            if(dish!=null){
+                dish.comments.push(req.body)//because dish is added as an array
+                dish.save()
+                .then((dish)=>{
+                    // send the staus code that we got it
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type','application/json');
+                })
+            }else{
+                err = new Error("Dish not found");
+                err.status = 404;
+                return next(err);
+            }
+    })
 })
 .put((req,res,next)=>{
     res.statusCode = 403;
-    res.end('PUT operation is nnot supported on /Dishes');
-}).delete((req,res,next)=>{
-    Dishes.remove({})
-    .then((resp)=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json(resp);
-    },(err)=>next(err)).catch((err)=>next(err));
+    res.send('PUT operation is not supported');
 })
 
-dishRouter.route('/:dishId')
-.get((req,res,next)=>{
-    Dishes.findById(req.params.dishId)
-    .then((dish)=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json(dish);
-    },(err)=>next(err)).catch((err)=>next(err));
-})
-.post((req,res,next)=>{
-    res.setHeader = 403;
-    res.end('PUT request is not supported');
-})
-.put((req,res,next)=>{
-    Dishes.findByIdAndUpdate(req.params.dishId,{
-        $set:req.body
-        // new:true will send the result afte update it
-        // and we catch it by a function
-    },{new:true}).then((dish)=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json(dish);
-
-    },(err)=>next(err)).catch((err)=>next(err));
-})
 .delete((req,res,next)=>{
-    Dishes.findByIdAndDelete(req.params.dishId)
-    .then((resp)=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json(resp);
-    },(err)=>next(err)).catch((err)=>next(err));
-});
+        // find if there is a dishes
+        Dishes.findById((req.params.dishId))
+        .then((dish)=>{
+            // check if it null or not
+            if(dish!=null){
+                // then delete every comments in that dish id
+                // you have to delete it through  a loop
+                for(var i = (dish.comments.length -1);i>=0;i--){
+                    // print all the comments id first    
+                    //console.log(dish.comments[i]._id)
+                    // just print a comment for testing purpose
+                    //console.log(dish.comments.id(dish.comments[i]._id));
+                    // we successfully get the comment id just delete it
+                    dish.comments.id(dish.comments[i]._id).remove();
+                }
+                dish.save()
+                .then((dish)=>{
+                    res.statusCode = 200;
+                    res.setHeader('Content=-Type','appliation/json');
+                    res.json(dish);
+                    // if everything goes right
+                    // if you test with the get requuest
+                    // yoiu wont get any comments
+                    // only empty array
 
-
-
+                },(err)=>{
+                    next(err);
+                });
+            }
+        })  
+})
 
 
 
