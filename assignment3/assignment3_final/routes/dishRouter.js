17f9100be3722e1def148dfb654e083101fa91a4
root <root@localhost.localdomain>
2019-11-27 21:56:07 +0000

    added

---
+++ assignment3/assignment3_final/routes/dishRouter.js
@@ -0,0 +1,354 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const dishRouter = express.Router();
+dishRouter.use(bodyParser.json());
+const Dishes = require('../models/dishes');
+const authenticate = require('../authenticate');
+
+
+
+// this is open for all any body can get access
+dishRouter.route('/')
+.get((req,res,next) => {
+    Dishes.find({})
+    .populate('comments.author')
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+
+// only the admin can post dishes
+.post(authenticate.verifyOrdinaryUser,authenticate.verifyAdmin,(req,res,next)=>{
+    Dishes.create(req.body)
+    .then((dish)=>{
+        console.log("Dish is created",dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+    },(err)=>next(err)).catch((err)=>next(err));
+})
+
+// does not matter
+// because it is not supported
+.put((req,res,next)=>{
+    res.statusCode = 403;
+    res.end('PUT operation is nnot supported on /Dishes');
+})
+// only the admin can delete the post
+.delete(authenticate.verifyOrdinaryUser,authenticate.verifyAdmin,(req,res,next)=>{
+    Dishes.remove({})
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    },(err)=>next(err)).catch((err)=>next(err));
+})
+
+// open for all any body can see a specfic dish
+dishRouter.route('/:dishId')
+.get((req,res,next) => {
+    Dishes.findById(req.params.dishId)
+    .populate('comments.author')
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+// does not matter cause it is ont supported
+.post((req,res,next)=>{
+    res.setHeader = 403;
+    res.end('POST request is not supported');
+})
+// only the admin can modify the dishes
+
+.put(authenticate.verifyOrdinaryUser,authenticate.verifyAdmin,(req,res,next)=>{
+    Dishes.findByIdAndUpdate(req.params.dishId,{
+        $set:req.body
+        // new:true will send the result afte update it
+        // and we catch it by a function
+    },{new:true}).then((dish)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+
+    },(err)=>next(err)).catch((err)=>next(err));
+})
+//only the admin can delete the dishes
+.delete(authenticate.verifyOrdinaryUser,authenticate.verifyAdmin,(req,res,next)=>{
+    Dishes.findByIdAndDelete(req.params.dishId)
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    },(err)=>next(err)).catch((err)=>next(err));
+});
+
+
+// open for all any body can see the comment
+dishRouter.route('/:dishId/comments')
+.get((req,res,next) => {
+    Dishes.findById(req.params.dishId)
+    .populate('comments.author')
+    .then((dish) => {
+        if (dish != null) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(dish.comments);
+        }
+        else {
+            err = new Error('Dish ' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+// only the varyfied user can post the comment
+.post((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        if (dish != null) {
+            req.body.author = req.user._id;
+            dish.comments.push(req.body);
+            dish.save()
+            .then((dish) => {
+                Dishes.findById(dish._id)
+                .populate('comments.author')
+                .then((dish) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(dish);
+                })            
+            }, (err) => next(err));
+        }
+        else {
+            err = new Error('Dish ' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+
+// does not matter it is not supported
+.put((req,res,next)=>{
+    res.statusCode = 403;
+    res.send('PUT operation is not supported');
+})
+
+
+// only the admin can delet all the comments of a specfic dish
+.delete(authenticate.verifyOrdinaryUser,authenticate.verifyAdmin,(req,res,next)=>{
+        // find if there is a dishes
+        Dishes.findById((req.params.dishId))
+        .then((dish)=>{
+            // check if it null or not
+            if(dish!=null){
+                // then delete every comments in that dish id
+                // you have to delete it through  a loop
+                for(var i = (dish.comments.length -1);i>=0;i--){
+                    // print all the comments id first    
+                    //console.log(dish.comments[i]._id)
+                    // just print a comment for testing purpose
+                    //console.log(dish.comments.id(dish.comments[i]._id));
+                    // we successfully get the comment id just delete it
+                    dish.comments.id(dish.comments[i]._id).remove();
+                }
+                dish.save()
+                .then((dish)=>{
+                    res.statusCode = 200;
+                    res.setHeader('Content=-Type','appliation/json');
+                    res.json(dish);
+                    // if everything goes right
+                    // if you test with the get requuest
+                    // yoiu wont get any comments
+                    // only empty array
+
+                },(err)=>{
+                    next(err);
+                });
+            }
+        })  
+})
+
+// open for all any body can see any specfiv comments
+dishRouter.route('/:dishId/comments/:commentId')
+.get((req,res,next) => {
+    Dishes.findById(req.params.dishId)
+    .populate('comments.author')    
+    .then((dish) => {
+        if (dish != null && dish.comments.id(req.params.commentId) != null) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(dish.comments.id(req.params.commentId));
+        }
+        else if (dish == null) {
+            err = new Error('Dish ' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Comment ' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err);            
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+// does not matter cause it is not supported
+.post((req,res,next0)=>{
+    res.statusCode = 403;
+    res.end('Not allowed');
+})
+
+// check the comment id and the user id and match it
+// if the matched get true then you can put
+// other wise not
+
+//if you do not put the veifyOrdinary middle ware 
+// you will not find the req.user so careful
+.put(authenticate.verifyOrdinaryUser, (req, res, next) => {
+    
+    console.log(req.user._id); // so we got the user id
+    
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        console.log(dish.comments.id(req.params.commentId).author)
+        if (dish != null && dish.comments.id(req.params.commentId) != null) {
+            if (req.body.rating) {
+                dish.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if (req.body.comment) {
+                dish.comments.id(req.params.commentId).comment = req.body.comment;                
+            }
+            dish.save()
+            .then((dish) => {
+                Dishes.findById(dish._id)
+                .populate('comments.author')
+                .then((dish) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(dish);  
+                })              
+            }, (err) => next(err));
+        }
+        else if (dish == null) {
+            err = new Error('Dish ' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Comment ' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err);            
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete( (req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        if (dish != null && dish.comments.id(req.params.commentId) != null) {
+
+            dish.comments.id(req.params.commentId).remove();
+            dish.save()
+            .then((dish) => {
+                Dishes.findById(dish._id)
+                .populate('comments.author')
+                .then((dish) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(dish);  
+                })               
+            }, (err) => next(err));
+        }
+        else if (dish == null) {
+            err = new Error('Dish ' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Comment ' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err);            
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+// operation for individual comment in a individual dish
+
+
+
+
+module.exports = dishRouter;
+
+
+
+
+
+
+// do it for the promoRouter and the leaderRouter too
+
+
+
+
+
+
+
+
+
+
+
+
+// with parameter
+
+
+
+// dishrouter.route('/:dishID')
+// .all((req,res,next)=>{
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type','text/plain')
+//     next();
+
+
+// })
+
+// .get((req,res,next)=>{
+//     res.end("will send all the dishes to you " +req.params.dishID + " to you ");
+// })
+
+
+// .post((req,res,next)=>{
+//     //we will fetch data in this post request
+//     //but for now we just send and text
+//     res.end("will add the dishes " +req.body.name + " with details "+req.body.description);
+//     // we wills end it with postman
+// })
+
+// .put((req,res,next)=>{
+
+//     res.write("updating the dishes....")
+//     res.end("will update the dish "+req.params.dishID + "with details "+req.body.description);
+// })
+// .delete((req,res,next)=>{
+//     res.end("Delete all the "+req.params.dishID+" dishes");
+// });
+
+
+
+
+
+
+
+//module.exports = dishrouter;
---
+++ assignment3/assignment3_final/routes/usersRouter.js
@@ -0,0 +1,139 @@
+var express = require('express');
+// import the user databse and also the 
+// import the databse the first
+var mongoose = require('mongoose');
+var User = require('../models/user');
+// now ewe need to parse the data  so we need the body parser
+var bodyParser = require('body-parser');
+// we need passport for this
+var passport = require('passport');
+var authenticate = require('../authenticate');
+
+// use the json response for the body parser
+
+
+// this is where i define the app
+// and it is the router
+var router = express.Router();
+
+/**
+ *  we change the login and signup functionality
+ *  for the app we using passport for this thing
+ * */
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      if (req.body.firstname)
+        user.firstname = req.body.firstname;
+      if (req.body.lastname)
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful!'});
+        });
+      });
+    }
+  });
+});
+
+
+
+// inthe registration we did both registration and authenticate
+// in the login we do just authenticate this
+// route,middleware,respose
+
+
+// in the previous we use th session for login
+// but now if the user is authenticated 
+// we will give a token 
+router.post('/login',passport.authenticate('local'),(req,res)=>{
+  
+  // if the user is successfully authenticate the user
+  // we get the usr object
+  var token = authenticate.getToken({_id:req.user.id}); // we create a object which is a user object but inside the user obj we only provide the -
+  // id cause thats allwe needed  
+  res.statusCode = 200;
+    res.setHeader('Content-Type','application/json');
+    
+    //pass the token to the client
+    res.json({
+        success:true,
+        status:'login successfull',
+        token:token
+    });
+});
+
+
+//*************VERY VERY IMPORTANT*******************//
+ /*
+ *  you may wonder why there is no else part
+ * if the authenticate is not successfull then what you will do?
+ * because if the authenticate is fauled then what we will do??
+ * well the interesting that if the authenticate is successfull
+ * then a variable
+ * req.user
+ * and
+ * req.password
+ *
+ * variable will autometically store the user and password
+ * if it is not successfull then this two variable will be empty
+ * this will use in the main app.js
+ * so we just search if the user and password is empty or nots
+ *
+  */
+
+//***************************************************//
+
+
+
+
+
+
+
+
+
+
+
+
+
+// logout process starts here
+router.get('/logout',(req,res)=>{
+  if(req.session){
+    req.session.destroy(); // destroy the sessions
+    // now lets the cookies be removed
+    res.clearCookie('session-id');
+    // sending back to the home page
+    res.redirect('/');
+  }else{
+    // if there is no session is already there
+    var err = new Error('You are not even logged in');
+    err.status = 403;
+    next(err);
+  }
+});
+
+router.get('/getalltheusers',(req,res,next)=>{
+    User.find({}).then((users)=>{
+      res.statusCode = 200;
+      res.setHeader('Content-Type','text/json');
+      res.json(users);
+    })
+});
+
+
+module.exports = router;
